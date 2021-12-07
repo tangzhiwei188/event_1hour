783e20a8fa65b45797f9a3228c6758152cf1c51a
tangzw_ucas <2841330740@qq.com>
2021-12-07 10:55:30 +0800

    Update event_1hour.py

--- event_1hour.py
+++ event_1hour.py
@@ -5,13 +5,23 @@ Created on Thu Nov 18 15:50:31 2021
 @author: ZIM
 """
 data=open("event_1hour_Sanhe.cat","w+")
-for month in range(5,6,1):
+for month in range(5,7,1):
     month='%02d' %month
-    for day in range(1,31,1):
+    for day in range(1,32,1):
         day='%02d' %day
-        if month=='6' and day=='31':
+        if str(month)=='02' and str(day)=='31' :
             continue
-        for hour in range(0,23,1):
+        if str(month)=='02' and str(day)=='30' :
+            continue
+        if str(month)=='04' and str(day)=='31' :
+            continue
+        if str(month)=='06' and str(day)=='31' :
+            continue
+        if str(month)=='09' and str(day)=='31' :
+            continue
+        if str(month)=='11' and str(day)=='31' :
+            continue
+        for hour in range(0,24,1):
             hour='%02d' %hour
             print('2021'+'/'+str(month)+'/'+str(day)+','+str(hour)+':'+'00:00'+' '+'41.00 101.00 10 1',file=data)
 data.close()
